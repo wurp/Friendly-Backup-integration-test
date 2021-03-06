e30daa52069795838c68dbdde5c3e7b233c0594e
Bobby Martin <bobbymartin2@gmail.com>
2021-03-06 09:24:02 -0800

    move installation code to Friendly-Backup

--- test.sh
+++ test.sh
@@ -8,14 +8,12 @@ export testdir=target/integ-working/happy1/client1
 export srcdir=test/integ/happy1/client1
 export fbdir=$(readlink -m ../Friendly-Backup)
 
-mkdir -p "$testdir"/bin/var
-cp -r "$fbdir"/dist/generic/* "$testdir"/bin/
+"$fbdir/install.sh" "$testdir"
 cp -r "$srcdir"/dir-to-backup "$testdir"
 cp -r "$srcdir"/gnupg "$testdir"
 cp "$srcdir"/BackupConfig.properties "$testdir"/bin/var/
-ln -s $(readlink -m "$fbdir"/target/Friendly-Backup-*-jar-with-dependencies.jar) "$testdir"/bin/
 cd "$testdir"/bin
-rm *.log
+rm -f *.log
 ./run.sh
 
 # mkdir -p target/integ-working/happy2
