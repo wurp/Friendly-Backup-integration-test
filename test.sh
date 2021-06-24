82b793a3ecce0cffc6f9002743cfa97768bf9639
Bobby Martin <bobbymartin2@gmail.com>
2021-06-23 22:55:45 -0700

    got tests working!

--- test.sh
+++ test.sh
@@ -25,52 +25,69 @@ rm -rf target/integ-working
 
 export fbdir=$(readlink -m ../Friendly-Backup)
 
-# Install and start up each client
-for srcdir in test/integ/happy1/client*
+for testbasedir in test/integ/happy*
 do
-  testdir=target/integ-working/happy1/$(basename $srcdir)
+  testname=$(basename $testbasedir)
 
-  # install FriendlyBackup
-  "$fbdir/install.sh" "$testdir"
+  # Install and start up each client
+  for srcdir in $testbasedir/client*
+  do
+    testdir=target/integ-working/$testname/$(basename $srcdir)
+  
+    # install FriendlyBackup
+    "$fbdir/install.sh" "$testdir"
+  
+    # Copy test client config, identity, etc.
+    [ -d "$srcdir"/dir-to-backup ] && cp -r "$srcdir"/dir-to-backup "$testdir"
+    cp -r "$srcdir"/gnupg "$testdir/var/"
+    cp "$srcdir"/BackupConfig.properties "$testdir"/var/
+  
+    # Clean logs & start process
+    pushd "$testdir"
+    rm -f *.log
+    ./bin/run.sh > ./run.out &
+    CLEANUPPID="$! $CLEANUPPID "
+    popd
+  done
+  
+  # wait for the clients to begin listening
+  sleep 1
+  cd "$RUNDIR"
+  TESTCLIENTBASE=target/integ-working/$testname/client1/
+  
+  # Start the backup
+  tail -f $TESTCLIENTBASE/service.log | sed '/Starting backup/ q' > /dev/null&
+  pid=$!
 
-  # Copy test client config, identity, etc.
-  cp -r "$srcdir"/dir-to-backup "$testdir"
-  cp -r "$srcdir"/gnupg "$testdir/bin/var/"
-  cp "$srcdir"/BackupConfig.properties "$testdir"/bin/var/
+  touch "$TESTCLIENTBASE/backup.txt"
+  
+  echo "Waiting for backup to start"
+  wait $pid
 
-  # Clean logs & start process
-  cd "$testdir"/bin
-  rm -f *.log
-  ./run.sh > /dev/null &
-  CLEANUPPID="$! $CLEANUPPID "
+  echo "Backup started"
+
+  tail -f $TESTCLIENTBASE/service.log | sed '/Backup complete/ q' > /dev/null
+  echo "Backup complete"
+  
+  tail -f $TESTCLIENTBASE/service.log | sed '/Starting restore/ q' > /dev/null&
+  pid=$!
+
+  touch "$TESTCLIENTBASE/restore.txt"
+  
+  echo "Waiting for restore to start"
+  wait $pid
+  echo "Restore started"
+  tail -f $TESTCLIENTBASE/service.log | sed '/Restore complete/ q' > /dev/null
+  echo "Restore complete"
+  
+  if diff -qr "$TESTCLIENTBASE/dir-to-backup/" "$TESTCLIENTBASE/restore-dir/"
+  then
+    echo "Test passed"
+  else
+    echo "Test failed - backup & restore files not the same" 1>&2
+  fi
 done
 
-# wait for the clients to begin listening
-sleep 1
-cd "$RUNDIR"
-TESTCLIENTBASE=target/integ-working/happy1/client1/
-
-# Start the backup
-touch "$TESTCLIENTBASE/bin/var/backup.txt"
-
-tail -f $TESTCLIENTBASE/bin/service.log | sed '/Starting backup/ q' > /dev/null
-echo "Backup started"
-tail -f $TESTCLIENTBASE/bin/service.log | sed '/Backup complete/ q' > /dev/null
-echo "Backup complete"
-
-touch "$TESTCLIENTBASE/bin/var/restore.txt"
-
-tail -f $TESTCLIENTBASE/bin/service.log | sed '/Starting restore/ q' > /dev/null
-echo "Restore started"
-tail -f $TESTCLIENTBASE/bin/service.log | sed '/Restore complete/ q' > /dev/null
-echo "Restore complete"
-
-if diff -qr "$TESTCLIENTBASE/dir-to-backup/" "$TESTCLIENTBASE/restore-dir/"
-then
-  echo "Test passed"
-else
-  echo "Test failed - backup & restore files not the same" 1>&2
-fi
 
 # mkdir -p target/integ-working/happy2
 cat  > /dev/null <<EOF
